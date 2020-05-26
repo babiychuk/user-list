2017864112649b2a1772e144de31b9765b056a3d
Евгений Бабийчук <e.babiychuk.ua@gmail.com>
2020-05-26 21:51:37 +0300

    Updates

---
+++ precache-manifest.aa015b69d9a6f7b60099f7990521ff64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80923bd7a923abf3a4a4a27d553fa935",
+    "url": "/users-list/index.html"
+  },
+  {
+    "revision": "82bfd877d49c96cdc786",
+    "url": "/users-list/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "29a202b28c262008dd21",
+    "url": "/users-list/static/css/main.16e2292c.chunk.css"
+  },
+  {
+    "revision": "82bfd877d49c96cdc786",
+    "url": "/users-list/static/js/2.217389fa.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/users-list/static/js/2.217389fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29a202b28c262008dd21",
+    "url": "/users-list/static/js/main.a34174a1.chunk.js"
+  },
+  {
+    "revision": "5a5843be38dce47bfce0",
+    "url": "/users-list/static/js/runtime-main.5c414424.js"
+  }
+]);
